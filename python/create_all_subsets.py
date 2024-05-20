d3f57c36e1e075c37cfe89d52a752f169d1ea831
tushargupta14 <tushariitkgp14@gmail.com>
2024-05-21 00:49:25 +0530

    code cleaned

--- python/create_all_subsets.py
+++ python/create_all_subsets.py
@@ -1,50 +1,29 @@
 # Leetcode: https://leetcode.com/problems/sum-of-all-subset-xor-totals/?envType=daily-question&envId=2024-05-20
-lass Solution:
+class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-
-
-        select = [0, 1]
         seen = set()
-
-        xor_so_far = 0
-        
-        idx_arr = ()
         self.xor_sum = 0 
 
         def create_subsets(nums, i, xor_so_far, seen, idx_arr):
-
-            
             if i >= len(nums):
-                self.xor_sum+= xor_so_far
-                #print(idx_arr, 'xor_sum', xor_so_far)
+                self.xor_sum += xor_so_far
                 seen.add(idx_arr)
                 return
-            #print(idx_arr)
+            
             if i == len(nums)-1:
-                
-                self.xor_sum+= xor_so_far
-                self.xor_sum+= xor_so_far ^ nums[i]
+                self.xor_sum += xor_so_far
+                self.xor_sum += xor_so_far ^ nums[i]
                 seen.add(idx_arr)
                 seen.add(idx_arr + (nums[i], ))
-
-                #print('---', idx_arr, 'xor_sum', xor_so_far ^ nums[i])
-                
                 return
-
-            #idx_arr = idx_arr + (nums[i], )
-            #print(idx_arr)
+            
             create_subsets(nums, i+1, xor_so_far, seen, idx_arr)
-            #if i <= len(nums[i])-1:
             xor_so_far ^= nums[i] 
             create_subsets(nums, i+1, xor_so_far, seen, idx_arr + (nums[i],))
 
             return
  
         create_subsets(nums, 0, 0, seen, ())
-        #print(seen)
-        #seen = set()
-        #print('Here')
-        #create_subsets(nums, 1, 0, seen, ())
 
         return self.xor_sum
 
